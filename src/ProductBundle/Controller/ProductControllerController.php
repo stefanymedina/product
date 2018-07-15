67508cb61fa3de3fed833d16961c19b33c08b4b9
smedina <stefanymedina25@gmail.com>
2018-07-15 15:27:52 -0400

    it is added new file for save a new product too it is added library of jquery and new file for styles

--- src/ProductBundle/Controller/ProductControllerController.php
+++ src/ProductBundle/Controller/ProductControllerController.php
@@ -5,17 +5,31 @@ namespace ProductBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ProductControllerController extends Controller
 {
+
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
-        $product = $em->getRepository('ProductBundle:Product')->findOneBy(array('id' => 1));
-        return $this->render('@Product/ProductController/index.html.twig', array(
-          'product' => $product
-            // ...
-        ));
+        return $this->render('@Product/ProductController/index.html.twig');
+    }
+
+
+    public function formCreaProducAction(){
+      $em = $this->getDoctrine()->getManager();
+      $category = $em->getRepository('ProductBundle:Category')->findAll();
+      return $this->render('@Product/ProductController/CreateProduct.html.twig', array(
+        'category' => $category
+      ));
+    }
+
+    public function laodProductAction($fatherId){
+      $em = $this->getDoctrine()->getManager();
+      $serializer = $this->get('jms_serializer');
+      $brand = $em->getRepository('ProductBundle:Brand')->findBy( array('category' => $fatherId), array('id'=>'asc') );
+      $brand = $serializer->serialize($brand, 'json');
+      return new JsonResponse(json_decode($brand, true), 200);
     }
 
 }
